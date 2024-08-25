d6cb20bfdff8b0a8da632a34244758744dddcba7
wildarte <wildarte10@gmail.com>
2024-08-25 17:12:30 -0300

    second commit

---
+++ single-post.php
@@ -0,0 +1,9 @@
+<?php get_header() ?>
+
+<main>
+    <section class="header_page" style="background-image: url(https://template-kit1.evonicmedia.com/layout37/wp-content/uploads/2023/08/banner-2.png);">
+        <h2><?= get_the_title(); ?></h2>
+    </section>
+</main>
+
+<?php get_footer() ?>
